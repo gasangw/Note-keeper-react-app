6bb25ec261d03e389be3dd9493500059a9cc896d
gasangw <thomasgasangwa@gmail.com>
2023-08-17 10:10:05 +0200

    Implement greeting functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 
 function App() {
+  let time = new Date(2023, 1,1,10)
+  
+
+  const customStyle = {
+    color: ''
+  }
+  
+  let greeting;
+  const showGreetings =() => {
+    let hour = time.getHours();
+    if(hour < 12){
+      greeting = 'Good Morning!'
+      customStyle.color = 'red'
+    } else if(hour < 18 ){
+     greeting = 'Good  Afternoon'
+     customStyle.color = 'green'
+    } else {
+      greeting = 'Good Night'
+      customStyle.color = 'blue'
+    }
+
+    return greeting;
+  }
   return (
     <div className="App">
-       <h1>Lets get started</h1>
+       <h1 style={customStyle}>{showGreetings()}</h1>
     </div>
   );
 }
